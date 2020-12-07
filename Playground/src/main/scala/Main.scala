b122f65020ee0cdd502fecfa328648dc7df4fc4f
ArtsiomKaliaha <stakkato95@gmail.com>
2020-12-07 12:50:00 +0100

    Add tuples, closures and lambdas

--- Playground/src/main/scala/Main.scala
+++ Playground/src/main/scala/Main.scala
@@ -1,9 +1,12 @@
 object Main {
   def main(args: Array[String]) = {
-//    assertions()
-//    classes()
-//    optional()
-    companionObjects()
+    //    assertions()
+    //    classes()
+    //    optional()
+    //    companionObjects()
+    //    tuples()
+    //    lambdas()
+    closures()
   }
 
   def assertions() = {
@@ -52,5 +55,59 @@ object Main {
   def companionObjects() = {
     println(Movie.getMovie(1999).get)
     println(Movie.getMovie(1998).getOrElse(new Movie("default", 2000)))
+  }
+
+  def tuples() = {
+    val t1 = Tuple2("hello", 1)
+    val t2 = ("hello", "world", 2)
+    println(t1._1)
+    println(t2._3)
+
+    val (hello, world, two) = t2
+    println(hello + " " + world + " " + two)
+  }
+
+  def lambdas() = {
+    def lambda = { x: Int => x + 1 }
+
+    println(lambda(4))
+
+    def lambda2 = (x: Int) => x + 2
+
+    println(lambda2(4))
+
+    def lambda3 = new Function1[Int, Int] {
+      override def apply(v1: Int): Int = v1 + 3
+    }
+
+    println(lambda3(4))
+
+    def lambda4 = new (Int => Int) {
+      override def apply(v1: Int): Int = v1 + 4
+    }
+
+    println(lambda4.apply(4))
+    println("is function taking one arg and returning one arg " + lambda4.isInstanceOf[Function1[_, _]])
+    println("is function taking one arg and returning one arg " + lambda4.isInstanceOf[(_) => _])
+  }
+
+  def closures() = {
+    var number = 1
+
+    def closure = (x: Int, y: Int) => x + y + number
+
+    println(closure(1, 2))
+
+    number = 5
+    println(closure(1, 2))
+
+    def addFive = (x: Int) => x + 10
+    def funReturningAnotherFun(x: Int, f: Int => Int) = f(x)
+    val resultOfAddingFiveAndTen = funReturningAnotherFun(5, addFive)
+    println(resultOfAddingFiveAndTen)
+
+    val list = List("HELLO", "WoRlD", "ExClAmAtIoN")
+    def listProcessor(ls: List[String], sideEffect: String => String) = ls map sideEffect
+    println(listProcessor(list, _.toLowerCase))
   }
 }
